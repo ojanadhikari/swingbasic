c722e3470c404f50b989134818e9164469688017
ojan <ojan@rebootedcl.com>
2026-06-23 22:53:52 +0545

    event done

---
+++ src/main/java/com/bo/event/MouseAdapterChild.java
@@ -0,0 +1,21 @@
+package com.bo.event;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MouseAdapterChild extends MouseAdapter {
+    private JLabel label;
+
+    public MouseAdapterChild(JLabel label) {
+        this.label = label;
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        label.setText("Mouse Pressed");
+    }
+}
+
+
+
